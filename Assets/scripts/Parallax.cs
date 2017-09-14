74ed5d6a5e9dc4d17c2400ac44a9ca40e7bd4f82
Lucas Soares <lsoares267@gmail.com>
2017-09-13 21:56:58 -0300

    Revert "excluindo os arquivos desnecessarios"
    This reverts commit 36f55adbf81becb1e185db680d79c6b491f3bc87.

---
+++ Assets/scripts/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Parallax : MonoBehaviour {
+	public RawImage image;
+	public float speed; 
+	Rect rect ; 
+
+	// Use this for initialization
+	void Start () { 
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		rect = image.uvRect; 
+		rect.x += speed * Time.deltaTime; 
+		image.uvRect = rect; 
+
+
+
+	}
+}
---
+++ Assets/scripts/Sobre.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Sobre : MonoBehaviour {
+
+    public Canvas instru;
+    public Canvas sobre;
+    
+
+	void Start () {
+        instru.enabled = false;
+        sobre.enabled = false;	
+	}
+	
+	void Update () {
+		
+	}
+
+    public void openAbout()
+    {
+        sobre.enabled = true;
+    }
+    public void openInstru()
+    {
+        instru.enabled = true;
+    }
+
+    public void CloseAbout()
+    {
+        sobre.enabled = false;
+    }
+
+    public void CloseInstru()
+    {
+        instru.enabled = false;
+    }
+}
